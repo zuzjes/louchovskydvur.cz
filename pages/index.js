0a17710abd3764d3a8e2f387291289748f5b9507
Zuzana Jeschke <zuz@zuzjes.com>
2021-03-22 19:16:26 +0100

    Add images

--- pages/index.js
+++ pages/index.js
@@ -20,29 +20,7 @@ const Header = styled.h1`
   font-size: 2.8em;
   margin-block-end: 0.2em;
 `;
-const Technologies = styled.b`
-  font-size: 1.7em;
-`;
-const A = styled.a`
-  color: #088958;
-`;
-const Button = styled.a`
-  border: 2px solid #088958;
-  padding: 0.15em 0.4em;
-  margin: 0.3em 0.6em 0.3em 0;
-  color: #088958;
-  text-decoration: none;
-  display: inline-block;
-  &:hover {
-    border: 2px solid #088958;
-    padding: 0.15em 0.4em;
-    background-color: #088958;
-    color: white;
-  }
-`;
-Button.defaultProps = {
-  target: "_blank"
-};
+
 
 export default () => {
   return (
