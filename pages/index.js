ef9235f3c4b88adbd856cfab5235d00d2d943cbb
Zuzana Jeschke <zuz@zuzjes.com>
2021-03-23 19:00:32 +0100

    Nav and HeaderBlock done

--- pages/index.js
+++ pages/index.js
@@ -2,55 +2,80 @@ import Head from "next/head";
 import Gauges from "../components/Gauges";
 import GoogleAnalytics from "../components/GoogleAnalytics";
 import styled, { createGlobalStyle } from "styled-components";
-import { Nav, Navbar } from "react-bootstrap"
+import { Nav, Navbar, Container } from "react-bootstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
+import LouchovLogo from "../assets/louchov_logo.svg";
 
 const GlobalStyle = createGlobalStyle`
   body {
-    font-size: 1.5em;
+    /* font-size: 1em; */
+    /* font-family: "Work Sans", sans-serif; */
   }
 `;
-const Container = styled.div`
-  padding: 0 2em;
-  font-family: "IBM Plex Sans", sans-serif;
-  max-width: 800px;
-  @media (max-width: 600px) {
-    padding: 0 1em;
+const A = styled.a`
+  color: #D24423;
+  cursor: pointer;
+  :hover {
+    color: #D24423;
   }
 `;
+
+const LouchovImg = styled.img`
+    position: relative;
+    margin-top: -3%;
+    margin-left: 24em;
+    margin-right: 24em;
+`;
+
 const Header = styled.h1`
-  font-size: 2.8em;
-  margin-block-end: 0.2em;
+  font-size: 4em;
+  padding: 1em 0 0.3em 0;
+  color: white;
+  text-align: center;
 `;
-
-
+const RedBackground = styled.div`
+  background-color: #D24423;
+`;
+const ButtonWhiteBorder = styled.a`
+  border: 1px solid white;
+  font-size: 14px;
+  padding: .5625em 1.2em;
+  margin: 0.3em 0.6em 0.3em 0;
+  color: white;
+  text-decoration: none;
+  display: inline-block;
+  &:hover {
+    border: 1px solid white;
+    background-color: white;
+    text-decoration: none;
+    color: #D24423;
+  }
+`;
 export default () => (
   <>
     <GlobalStyle />
     <Head>
-      <title>louchovsky dvur</title>
+      <title>Louchovský dvůr</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link
+      {/* <link
         href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap"
         rel="stylesheet"
-      />
+      /> */}
+      <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;700&display=swap" rel="stylesheet" />
     </Head>
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">Louchov</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
+    <Navbar className="pt-3 pb-3">
+      <A href="/">Louchovský dvůr</A>
+      <Nav className="ml-auto">
+        <A href="#contact">Kontakt</A>
+      </Nav>
     </Navbar>
-    <Container>
-
-      <Header>Louchovsky dvur</Header>
-
-    </Container>
-
+    <RedBackground>
+      <Container className="text-center pb-5" >
+        <LouchovImg src={LouchovLogo} />
+        <Header>"Kde je kráva krávou"</Header>
+        <ButtonWhiteBorder href="mailto:">chci masooo</ButtonWhiteBorder>
+      </Container>
+    </RedBackground>
     <Gauges gauges_site_id="xx" />
     <GoogleAnalytics google_analytics_site_id="xx" />
   </>
